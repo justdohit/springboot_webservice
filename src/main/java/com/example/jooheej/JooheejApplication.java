522d3cb2b3d806d23d5fa35a8f0aa63fe27143e9
주희정 <jooheejung@juhuijeong-ui-iMac.local>
2023-01-21 15:23:23 +0900

    Initial commit

---
+++ src/main/java/com/example/jooheej/JooheejApplication.java
@@ -0,0 +1,13 @@
+package com.example.jooheej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooheejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooheejApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jooheej/JooheejApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jooheej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooheejApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
